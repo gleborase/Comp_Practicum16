d5c00ef240dfc8f410d749e25ad48b633ed30449
gleborase <krasnopolsci@gmail.com>
2025-01-26 21:50:13 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{/// <summary>
+ /// Задание 4. Вывести на экран наклонную линию (\) из 8 символов.
+ /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("#");              
+            }
+            Console.ReadKey();
+        }
+    }
+}
